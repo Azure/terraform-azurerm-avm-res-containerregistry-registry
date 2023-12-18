6acf239dbc903b04d719b2095c360c8aeb013b95
Stu <kewalaka@gmail.com>
2023-12-19 10:58:17 +1300

    fixes requested during avm team review (#4)
    * fix: requested fixes from AVM team
    
    * fix: reference to managed identity

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,20 @@
+# Minimum required outputs
+# https://azure.github.io/Azure-Verified-Modules/specs/shared/#id-rmfr7---category-outputs---minimum-required-outputs
+output "resource_id" {
+  description = "The resource id for the parent resource."
+  value       = azurerm_container_registry.this.id
+}
+
+output "name" {
+  description = "The name of the parent resource."
+  value       = azurerm_container_registry.this.name
+}
+
+output "system_assigned_mi_principal_id" {
+  description = "The system assigned managed identity principal ID of the parent resource."
+  value       = try(azurerm_container_registry.this.identity[0].principal_id, null)
+}
+
 output "private_endpoints" {
   description = "A map of private endpoints. The map key is the supplied input to var.private_endpoints. The map value is the entire azurerm_private_endpoint resource."
   value       = azurerm_private_endpoint.this
