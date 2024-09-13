3b56d74d8b42f54499a6aa759e82d9c142c29301
Thanos Lefteris <alefteris@gmail.com>
2024-09-14 00:35:09 +0300

    Option to not manage the dns zone group in private endpoints (#81)

--- main.privateendpoint.tf
+++ main.privateendpoint.tf
@@ -1,5 +1,6 @@
+# The PE resource when we are managing the private_dns_zone_group block:
 resource "azurerm_private_endpoint" "this" {
-  for_each = var.private_endpoints
+  for_each = { for k, v in var.private_endpoints : k => v if var.private_endpoints_manage_dns_zone_group }
 
   location                      = coalesce(each.value.location, var.location)
   name                          = each.value.name != null ? each.value.name : "pe-${var.name}"
@@ -31,6 +32,39 @@ resource "azurerm_private_endpoint" "this" {
       name                 = each.value.private_dns_zone_group_name
       private_dns_zone_ids = each.value.private_dns_zone_resource_ids
     }
+  }
+}
+
+# The PE resource when we are **not** managing the private_dns_zone_group block, such as when using Azure Policy:
+resource "azurerm_private_endpoint" "this_unmanaged_dns_zone_groups" {
+  for_each = { for k, v in var.private_endpoints : k => v if !var.private_endpoints_manage_dns_zone_group }
+
+  location                      = coalesce(each.value.location, var.location)
+  name                          = each.value.name != null ? each.value.name : "pe-${var.name}"
+  resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
+  subnet_id                     = each.value.subnet_resource_id
+  custom_network_interface_name = each.value.network_interface_name
+  tags                          = each.value.tags
+
+  private_service_connection {
+    is_manual_connection           = false
+    name                           = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "pse-${var.name}"
+    private_connection_resource_id = azurerm_container_registry.this.id
+    subresource_names              = ["registry"]
+  }
+  dynamic "ip_configuration" {
+    for_each = each.value.ip_configurations
+
+    content {
+      name               = ip_configuration.value.name
+      private_ip_address = ip_configuration.value.private_ip_address
+      member_name        = "registry"
+      subresource_name   = "registry"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [private_dns_zone_group]
   }
 }
 
