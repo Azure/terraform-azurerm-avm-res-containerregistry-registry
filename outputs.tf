3681ecfc66791849251a4dcef1ff222aa21977f2
Chua Ming Kiang <mingk@Chuas-MacBook-Pro.local>
2025-03-29 09:54:08 +0800

    Add examples and output

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,21 @@ output "private_endpoints" {
   value       = var.private_endpoints_manage_dns_zone_group ? azurerm_private_endpoint.this : azurerm_private_endpoint.this_unmanaged_dns_zone_groups
 }
 
+output "scope_maps" {
+  description = <<DESCRIPTION
+A map of scope map keys to scope map values. Each scope map value is the entire azurerm_container_registry_scope_map resource.
+
+The scope map value contains the following attributes:
+- id: The Container Registry Scope Map ID
+- name: The name of the Container Registry Scope Map
+- resource_group_name: The name of the resource group in which the Container Registry Scope Map is created
+- container_registry_name: The name of the Container Registry associated with the Scope Map
+- actions: The list of actions assigned to the Scope Map
+- description: The description of the Container Registry Scope Map
+DESCRIPTION
+  value       = module.scope_maps
+}
+
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource" {
